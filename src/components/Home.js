6aabdc65e8bf29a70bd713e46997d4fe38cc35a2
Tyler Wagner <wagnerjt@email.sc.edu>
2019-05-18 13:47:04 -0400

    Created the known algorithm categories as an array to be mapped over for rendering the menu items easier

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,121 +1,110 @@
-import {Button, Card, CardGroup, Container, Header, Icon} from "semantic-ui-react"
-import React, {Component} from 'react'
+import React, { Component } from 'react';
+import {
+  Button,
+  Card,
+  CardGroup,
+  Container,
+  Header,
+  Icon,
+} from 'semantic-ui-react';
 
-import { selectGroup } from "../ducks/algorithms"
-import {connect} from "react-redux";
+import { selectGroup } from '../ducks/algorithms';
+import { connect } from 'react-redux';
+
+/**
+ * @author wagnerjt
+ * Contains a list of all of the Algorithms NEuler has a UI for
+ */
+const AlgorithmCategories = [
+  {
+    key: 'centralities',
+    header: 'Centralities',
+    description: `These algorithms determine the importance of distinct nodes in 
+      a network.`,
+    icon: 'sitemap',
+    group: 'Centralities',
+  },
+  {
+    key: 'communityDetection',
+    header: 'Community Detection',
+    description: `These algorithms evaluate how a group is clustered or
+                  partitioned, as well as its tendency to strengthen or break
+                  apart.`,
+    icon: 'sitemap',
+    group: 'Community Detection',
+  },
+  {
+    key: 'pathFinding',
+    header: 'Path Finding',
+    description: `These algorithms help find the shortest path or evaluate the
+                  availability and quality of routes.`,
+    icon: 'sitemap',
+    group: 'Path Finding',
+  },
+  {
+    key: 'similarity',
+    header: 'Similarity',
+    description: 'These algorithms help calculate the similarity of nodes.',
+    icon: 'sitemap',
+    group: 'Similarity',
+  },
+];
 
 class Home extends Component {
+  render() {
+    const containerStyle = {
+      padding: '1em',
+    };
 
+    const { selectGroup } = this.props;
 
-    render() {
-        const containerStyle = {
-            padding: '1em'
-        }
+    return (
+      <div style={containerStyle}>
+        <Container fluid>
+          <Header as={'h3'}>Algorithm Categories</Header>
+          <p>
+            The Neo4j Graph Algorithms Library supports the following categories
+            of algorithms.
+          </p>
 
-        const {selectGroup} = this.props
-
-        return (<div style={containerStyle}>
-                <Container fluid>
-                    <Header as={"h3"}>
-                        Algorithm Categories
-                    </Header>
-                    <p>
-                        The Neo4j Graph Algorithms Library supports the following categories of algorithms.
-                    </p>
-
-                    <CardGroup>
-                        <Card key={"centralities"}>
-                            <Card.Content>
-                                <Icon name='sitemap'/>
-                                <Card.Header>
-                                    Centralities
-                                </Card.Header>
-                                <Card.Meta>
-                                    These algorithms determine the importance of distinct nodes in a network
-                                </Card.Meta>
-                            </Card.Content>
-                            <Card.Content extra>
-                                <div className='ui two buttons'>
-                                    <Button basic color='green' onClick={() => selectGroup('Centralities')}>
-                                        Select
-                                    </Button>
-                                </div>
-                            </Card.Content>
-                        </Card>
-
-                        <Card key={"communityDetection"}>
-                            <Card.Content>
-                                <Icon name='sitemap'/>
-                                <Card.Header>
-                                    Community Detection
-                                </Card.Header>
-                                <Card.Meta>
-                                    These algorithms evaluate how a group is clustered or partitioned, as
-                                    well as its tendency to strengthen or break apart
-                                </Card.Meta>
-                            </Card.Content>
-                            <Card.Content extra>
-                                <div className='ui two buttons'>
-                                    <Button basic color='green' onClick={() => selectGroup('Community Detection')}>
-                                        Select
-                                    </Button>
-                                </div>
-                            </Card.Content>
-                        </Card>
-
-                        <Card key={"pathFinding"}>
-                            <Card.Content>
-                                <Icon name='sitemap'/>
-                                <Card.Header>
-                                    Path Finding
-                                </Card.Header>
-                                <Card.Meta>
-                                    These algorithms help find the shortest path or evaluate the availability and quality of routes
-                                </Card.Meta>
-                            </Card.Content>
-                            <Card.Content extra>
-                                <div className='ui two buttons'>
-                                    <Button basic color='green' onClick={() => selectGroup("Path Finding")}>
-                                        Select
-                                    </Button>
-                                </div>
-                            </Card.Content>
-                        </Card>
-
-                        <Card key={"similairty"}>
-                            <Card.Content>
-                                <Icon name='sitemap'/>
-                                <Card.Header>
-                                    Similarity
-                                </Card.Header>
-                                <Card.Meta>
-                                    These algorithms help calculate the similarity of nodes.
-                                </Card.Meta>
-                            </Card.Content>
-                            <Card.Content extra>
-                                <div className='ui two buttons'>
-                                    <Button basic color='green' onClick={() => selectGroup("Similarity")}>
-                                        Select
-                                    </Button>
-                                </div>
-                            </Card.Content>
-                        </Card>
-
-                    </CardGroup>
-
-                </Container>
-            </div>
-        )
-
-    }
+          {/* Render all known algorithm categories in their respective cards */}
+          <CardGroup>
+            {AlgorithmCategories.map(category => (
+              <Card key={category.key}>
+                <Card.Content>
+                  <Icon name={category.icon} />
+                  <Card.Header>{category.header}</Card.Header>
+                  <Card.Meta>{category.description}</Card.Meta>
+                </Card.Content>
+                <Card.Content extra>
+                  <div className="ui two buttons">
+                    <Button
+                      basic
+                      color="green"
+                      onClick={() => selectGroup(category.group)}
+                    >
+                      Select
+                    </Button>
+                  </div>
+                </Card.Content>
+              </Card>
+            ))}
+          </CardGroup>
+        </Container>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => ({
-    activeGroup: state.algorithms.group
-})
+  activeGroup: state.algorithms.group,
+});
 
 const mapDispatchToProps = dispatch => ({
-    selectGroup: group => dispatch(selectGroup(group))
-})
+  selectGroup: group => dispatch(selectGroup(group)),
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(Home)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Home);
--- src/index.js
+++ src/index.js
@@ -1,39 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
-import { PersistGate } from 'redux-persist/integration/react'
-import { Loader, Dimmer } from 'semantic-ui-react'
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage';
+import { Loader, Dimmer } from 'semantic-ui-react';
 
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore } from "redux"
-import { Provider } from 'react-redux'
-import rootReducer from './ducks'
+import rootReducer from './ducks';
 
 const persistConfig = {
   key: 'root',
   storage,
-  whitelist: ['settings']
-}
+  whitelist: ['settings'],
+};
 
-const persistedReducer = persistReducer(persistConfig, rootReducer)
-const store = createStore(persistedReducer)
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const store = createStore(persistedReducer);
 
-const persistor = persistStore(store)
+const persistor = persistStore(store);
 
-const LoaderComponent = <Dimmer active>
-  <Loader size='massive'>Connecting</Loader>
-</Dimmer>
+const LoaderComponent = (
+  <Dimmer active>
+    <Loader size="massive">Connecting</Loader>
+  </Dimmer>
+);
 
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate loading={LoaderComponent} persistor={persistor}>
-      <App/>
+      <App />
     </PersistGate>
-  </Provider>
-, document.getElementById('root'));
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
